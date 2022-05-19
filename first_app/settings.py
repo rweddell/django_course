7523a241071fe5bd6fffe427f19868ce4e7d3774
rweddell <rweddell4@gmail.com>
2022-05-19 09:04:30 -0500

    removing risk

--- first_app/settings.py
+++ first_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+with open("./access.json") as file:
+    credentials = json.load(file)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-b=3(ih*x*0r6)8uio#@atnkxb)0l**_7v5wte0n7grri7ujc@m"
+SECRET_KEY = credentials["secret_key"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
